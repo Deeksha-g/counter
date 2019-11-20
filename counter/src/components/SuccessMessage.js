a40275b4fc7831d11c392a0ebbe63823004cd6e5
Deeksha <deekshagbhat@gmail.com>
2019-11-20 17:12:25 +0530

    added border to success message

--- counter/src/components/SuccessMessage.js
+++ counter/src/components/SuccessMessage.js
@@ -4,13 +4,20 @@ import {StyleSheet, View, Text} from 'react-native';
 const SuccessMessage = props => {
   const {value} = props;
   return (
-    <View>
+    <View style={styles.messageSection}>
       <Text style={styles.text}>{value}</Text>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  messageSection: {
+    borderWidth: 1,
+    borderColor: '#3289a8',
+    backgroundColor: 'white',
+    borderRadius: 20,
+    marginTop: 10,
+  },
   text: {
     padding: 10,
     color: '#3269a8',
